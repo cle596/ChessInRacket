eb436d4ecedcda5b48423bdde5d2d5e02916873d
ch <changhanlee@gmail.com>
2016-07-21 11:20:49 -0400

    shit

--- main.rkt
+++ main.rkt
@@ -14,4 +14,56 @@
     (let ([i (update n (in (read-line)))])
       (loop (update i (car (spawn 8 i)))))))
 
-(loop root)
+;(loop root)
+
+(define (pront x)
+  (displayln x)
+  (flush-output))
+
+(define (writ x)
+  (call-with-output-file "/home/ch/Documents/racketess/x.txt"
+    (lambda (out)
+      (display (string-append x (string #\newline)) out)) #:mode 'text #:exists 'append))
+;(open-output-file "./x.txt" #:mode 'text #:exists 'append)
+
+(define (gui n)
+  (let ([i (read-line)])
+    (writ i)
+    (case i
+      [("xboard") (gui n)]
+      [("protover 2")
+       (begin
+         (pront (string-append
+                 "feature "
+                 ;"usermove=1 "
+                 "setboard=0 "
+                 "sigint=0 "
+                 "variants='normal' "
+                 "done=1"))
+         (gui n))]
+      [("go")
+       (let ([m (ttrans (car (spawn 8 n)))])
+         (pront (string-append "move " m))
+         (gui-loop (update n (rtrans m)) "white"))]
+      ;[("usermove") (pront (string-append "move " (car (spawn 8 n))))]
+      [else (if (regexp-match #rx"[a-h][1-8][a-h][1-8]" i)
+                (gui-loop (update n (rtrans i)) "black")
+                (void)
+                )]
+      ))
+  (gui n))
+
+(define (gui-loop n c)
+  (if (equal? c "white")
+      (let ([i (read-line)])
+        (if (regexp-match #rx"[a-h][1-8][a-h][1-8]" i)
+            (gui-loop (update n (rtrans i)) "black")
+            (gui-loop n "white")))
+      (let ([x (ttrans (car (spawn 8 n)))])
+        (pront (string-append "move " x))
+        (gui-loop (update n (rtrans x)) "white")
+        )
+      )
+  )
+
+(gui root)
--- util.rkt
+++ util.rkt
@@ -1,7 +1,7 @@
 (module util racket
   
   (require "node.rkt")
-  (provide pretty)
+  (provide (all-defined-out))
   
   (define (unicfy lst)
     (map (lambda (x)
@@ -18,8 +18,49 @@
      #\r #\u265C #\b #\u265D
      #\n #\u265E #\p #\u265F))
   
-  #|
-(define root
-  (node i_b #t 0 '()))
-|#
+  (define trans_hash
+    (hash
+     20 #\8 30 #\7
+     40 #\6 50 #\5
+     60 #\4 70 #\3
+     80 #\2 90 #\1
+     1 #\a 2 #\b
+     3 #\c 4 #\d
+     5 #\e 6 #\f
+     7 #\g 8 #\h))
+  
+  (define rtrans_hash
+    (hash
+     #\8 20 #\7 30
+     #\6 40 #\5 50
+     #\4 60 #\3 70
+     #\2 80 #\1 90
+     #\a 1 #\b 2
+     #\c 3 #\d 4
+     #\e 5 #\f 6
+     #\g 7 #\h 8))
+  
+  (define (pair-to-list x)
+    (list (car x) (cdr x)))
+  
+  (define (trans x)
+    (string
+     (hash-ref trans_hash (remainder x 10))
+     (hash-ref trans_hash (* (quotient x 10) 10))))
+  
+  (define (ttrans x)
+    (apply string-append (map trans (pair-to-list x))))
+  
+  (define (rtrans x)
+    (cons 
+    (for/sum ([x (list (hash-ref rtrans_hash (string-ref x 0)) (hash-ref rtrans_hash (string-ref x 1)))]) x)
+    (for/sum ([x (list (hash-ref rtrans_hash (string-ref x 2)) (hash-ref rtrans_hash (string-ref x 3)))]) x)))
+  
+  
+  
+  
+  
   )
+
+
+
